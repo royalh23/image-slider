26236244015ffe27510456b70ad7b337d52ffae3
Royal <royalhuseyn902@gmail.com>
2024-01-04 22:50:10 +0400

    Make functions work for prev and next ims

--- src/index.js
+++ src/index.js
@@ -6,25 +6,19 @@ import './style.css';
   const rightArrow = document.querySelector('.right-arrow');
 
   function showPrevImg() {
-    images.forEach((img) => {
-      if (img.classList.contains('visible')) {
-        img.classList.toggle('visible');
-        let prevImgIndex = images.indexOf(img) - 1;
-        if (prevImgIndex === -1) prevImgIndex = 2;
-        images[prevImgIndex].classList.toggle('visible');
-      }
-    });
+    const currentImg = document.querySelector('.visible');
+    currentImg.classList.toggle('visible');
+    let prevImgIndex = images.indexOf(currentImg) - 1;
+    if (prevImgIndex === -1) prevImgIndex = 2;
+    images[prevImgIndex].classList.toggle('visible');
   }
 
   function showNextImg() {
-    images.forEach((img) => {
-      if (img.classList.contains('visible')) {
-        img.classList.toggle('visible');
-        let nextImgIndex = images.indexOf(img) + 1;
-        if (nextImgIndex === 3) nextImgIndex = 0;
-        images[nextImgIndex].classList.toggle('visible');
-      }
-    });
+    const currentImg = document.querySelector('.visible');
+    currentImg.classList.toggle('visible');
+    let nextImgIndex = images.indexOf(currentImg) + 1;
+    if (nextImgIndex === 3) nextImgIndex = 0;
+    images[nextImgIndex].classList.toggle('visible');
   }
 
   leftArrow.addEventListener('click', showPrevImg);
