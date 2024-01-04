1703beba3da36b89895185b2d9af417669db3dd8
Royal <royalhuseyn902@gmail.com>
2024-01-04 22:37:51 +0400

    Add event listeners to arrows for showing prev and next imgs

--- src/index.js
+++ src/index.js
@@ -2,6 +2,19 @@ import './style.css';
 
 (() => {
   const images = Array.from(document.querySelectorAll('.image-slides > img'));
+  const leftArrow = document.querySelector('.left-arrow');
+  const rightArrow = document.querySelector('.right-arrow');
+
+  function showPrevImg() {
+    images.forEach((img) => {
+      if (img.classList.contains('visible')) {
+        img.classList.toggle('visible');
+        let prevImgIndex = images.indexOf(img) - 1;
+        if (prevImgIndex === -1) prevImgIndex = 2;
+        images[prevImgIndex].classList.toggle('visible');
+      }
+    });
+  }
 
   function showNextImg() {
     images.forEach((img) => {
@@ -14,14 +27,6 @@ import './style.css';
     });
   }
 
-  function showPrevImg() {
-    images.forEach((img) => {
-      if (img.classList.contains('visible')) {
-        img.classList.toggle('visible');
-        let prevImgIndex = images.indexOf(img) - 1;
-        if (prevImgIndex === -1) prevImgIndex = 2;
-        images[prevImgIndex].classList.toggle('visible');
-      }
-    });
-  }
+  leftArrow.addEventListener('click', showPrevImg);
+  rightArrow.addEventListener('click', showNextImg);
 })();
