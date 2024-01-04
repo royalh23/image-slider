ae165de03613b450ec8c3ff63283178a736a0b66
Royal <royalhuseyn902@gmail.com>
2024-01-04 22:35:24 +0400

    Write funcs for showing previous and next images

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import './style.css';
+
+(() => {
+  const images = Array.from(document.querySelectorAll('.image-slides > img'));
+
+  function showNextImg() {
+    images.forEach((img) => {
+      if (img.classList.contains('visible')) {
+        img.classList.toggle('visible');
+        let nextImgIndex = images.indexOf(img) + 1;
+        if (nextImgIndex === 3) nextImgIndex = 0;
+        images[nextImgIndex].classList.toggle('visible');
+      }
+    });
+  }
+
+  function showPrevImg() {
+    images.forEach((img) => {
+      if (img.classList.contains('visible')) {
+        img.classList.toggle('visible');
+        let prevImgIndex = images.indexOf(img) - 1;
+        if (prevImgIndex === -1) prevImgIndex = 2;
+        images[prevImgIndex].classList.toggle('visible');
+      }
+    });
+  }
+})();
